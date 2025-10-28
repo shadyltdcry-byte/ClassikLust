d953eeb933639ea50a9ef0a3779bffdc17ac7ea7
shadyltdcry-byte <shadyltdcry@gmail.com>
2025-10-28 11:30:52 -0400

    Create server entry using new app with extras routes

---
+++ server/main.ts
@@ -0,0 +1,9 @@
+import app from './app';
+import http from 'http';
+
+const server = http.createServer(app);
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`[SERVER] Game server running on port ${PORT}`);
+});
