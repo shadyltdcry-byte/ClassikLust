52a800c06ef3123f90b20533c8bd690fd6616c68
shadyltdcry-byte <shadyltdcry@gmail.com>
2025-10-28 11:43:09 -0400

    Remove server/main.ts - using existing server/index.ts structure

--- server/main.ts
+++
@@ -1,9 +0,0 @@
-import app from './app';
-import http from 'http';
-
-const server = http.createServer(app);
-const PORT = process.env.PORT || 5000;
-
-server.listen(PORT, () => {
-  console.log(`[SERVER] Game server running on port ${PORT}`);
-});
